eb4935beb46d884f75c9dd81f532b55d70a12a13
Sean Callanan <l337.h4x0r@gmail.com>
2020-01-19 05:59:02 +0000

    Fixed jshint errors in index.js
    jshint identified missing semicolons throughout.

--- index.js
+++ index.js
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// jshint esversion: 8
+
 function generateUid() {
   if (!this.next_uid) {
     this.next_uid = 0;
@@ -19,40 +21,40 @@ function generateUid() {
   return (this.next_uid++);
 }
 
-const input_select = document.getElementById("input")
-const output_select = document.getElementById("output")
+const input_select = document.getElementById("input");
+const output_select = document.getElementById("output");
 
 function registerInput(name) {
-  const input = document.createElement("option")
-  input.innerHTML = name
-  input.uid = generateUid()
-  input_select.options.add(input)
+  const input = document.createElement("option");
+  input.innerHTML = name;
+  input.uid = generateUid();
+  input_select.options.add(input);
   return input.uid;
 }
 
 function removeFromSelect(select, uid) {
   for (i = 0; i < select.options.length; ++i) {
     if (select.options[i].uid == uid) {
-      select.options.remove(i)
-      return
+      select.options.remove(i);
+      return;
     }
   }
 }
 
 function unregisterInput(uid) {
-  removeFromSelect(input_select, uid)
+  removeFromSelect(input_select, uid);
 }
 
 function unregisterOutput(uid) {
-  removeFromSelect(output_select, uid)
+  removeFromSelect(output_select, uid);
 }
 
 function registerOutput(name, fn) {
-  const output = document.createElement("option")
+  const output = document.createElement("option");
   output.innerHTML = name;
   output.uid = generateUid();
   output.fn = fn;
-  output_select.options.add(output)
+  output_select.options.add(output);
   return output.uid;
 }
 
@@ -66,29 +68,29 @@ function dispatchMIDI(input_uid, data) {
 
 async function createVirtualKeyboard() {
   const keys =
-        ['a','w','s','d','r','f','t','g','h','u','j','i','k','o','l',';','[',"'",']']
+        ['a','w','s','d','r','f','t','g','h','u','j','i','k','o','l',';','[',"'",']'];
   const isWhite =
-        [ 1,  0,  1,  1,  0,  1,  0,  1,  1,  0,  1,  0,  1,  0,  1,  1,  0,  1,  0 ]
+        [ 1,  0,  1,  1,  0,  1,  0,  1,  1,  0,  1,  0,  1,  0,  1,  1,  0,  1,  0 ];
 
-  const keyboard = document.getElementById("keyboard")
-  keyboard.style.height = "100px"
-  keyboard.style.width = "400px"
-  keyboard.style.position = "relative"
+  const keyboard = document.getElementById("keyboard");
+  keyboard.style.height = "100px";
+  keyboard.style.width = "400px";
+  keyboard.style.position = "relative";
 
-  const numWhiteKeys = arraySum(isWhite)
-  const keyWidth = (100 / numWhiteKeys)
+  const numWhiteKeys = arraySum(isWhite);
+  const keyWidth = (100 / numWhiteKeys);
   
   function arraySum(a) {
     function sum(a,b) {
-      return a+b
+      return a+b;
     }
-    return a.reduce(sum, 0)
+    return a.reduce(sum, 0);
   }
 
   function keyLeft(i) {
-    const numWhiteKeysBefore = arraySum(isWhite.slice(0,i))
+    const numWhiteKeysBefore = arraySum(isWhite.slice(0,i));
     return (numWhiteKeysBefore * keyWidth) -
-      (isWhite[i] ? 0 : (keyWidth/2))
+      (isWhite[i] ? 0 : (keyWidth/2));
   }
   
   const input = registerInput("Virtual keyboard");
@@ -100,81 +102,81 @@ async function createVirtualKeyboard() {
   
   function makeMouseDownHandler(key) {
     return function(e) {
-      dispatchMIDI(input, makeMIDIData(channel, key, 64))
-    }
+      dispatchMIDI(input, makeMIDIData(channel, key, 64));
+    };
   }
 
   function makeMouseUpHandler(key) {
     return function(e) {
-      dispatchMIDI(input, makeMIDIData(channel, key, 0))
-    }
+      dispatchMIDI(input, makeMIDIData(channel, key, 0));
+    };
   }
 
   function contextMenuHandler(e) {
     if(e.preventDefault != undefined) {
-      e.preventDefault()
+      e.preventDefault();
     }
     if(e.stopPropagation != undefined) {
-      e.stopPropagation()
+      e.stopPropagation();
     }
-    return false
+    return false;
   }
 
   for (var i = 0; i < keys.length; ++i) {
-    const key = document.createElement("div")
-    keyboard.appendChild(key)
+    const key = document.createElement("div");
+    keyboard.appendChild(key);
 
-    const span = document.createElement("span")
-    key.appendChild(span)
+    const span = document.createElement("span");
+    key.appendChild(span);
 
-    const text = document.createTextNode(keys[i])
-    span.appendChild(text)
+    const text = document.createTextNode(keys[i]);
+    span.appendChild(text);
 
-    key.style.position = "absolute"
-    key.style.display = "flex"
-    key.style.justifyContent = "center"
-    key.style.alignItems = "flex-end"
-    key.style.left = keyLeft(i).toString() + "%"
-    key.style.bottom = isWhite[i] ? "0%" : "50%"
-    key.style.height = isWhite[i] ? "100%" : "50%"
-    key.style.width = keyWidth.toString() + "%"
-    key.style.borderStyle = "solid"
-    key.style.borderWidth = "thin"
-    key.style.borderColor = isWhite[i] ? "black" : "white"
-    key.style.color = isWhite[i] ? "black" : "white"
-    key.style.backgroundColor = isWhite[i] ? "white" : "black"
-    key.style.zIndex = isWhite[i] ? 0 : 1
+    key.style.position = "absolute";
+    key.style.display = "flex";
+    key.style.justifyContent = "center";
+    key.style.alignItems = "flex-end";
+    key.style.left = keyLeft(i).toString() + "%";
+    key.style.bottom = isWhite[i] ? "0%" : "50%";
+    key.style.height = isWhite[i] ? "100%" : "50%";
+    key.style.width = keyWidth.toString() + "%";
+    key.style.borderStyle = "solid";
+    key.style.borderWidth = "thin";
+    key.style.borderColor = isWhite[i] ? "black" : "white";
+    key.style.color = isWhite[i] ? "black" : "white";
+    key.style.backgroundColor = isWhite[i] ? "white" : "black";
+    key.style.zIndex = isWhite[i] ? 0 : 1;
 
-    const downHandler = makeMouseDownHandler(57 + i)
-    const upHandler = makeMouseUpHandler(57+ i)
+    const downHandler = makeMouseDownHandler(57 + i);
+    const upHandler = makeMouseUpHandler(57+ i);
 
-    key.addEventListener('mousedown', downHandler)
-    key.addEventListener('mouseup', upHandler)
-    key.addEventListener('touchstart', downHandler)
-    key.addEventListener('touchend', upHandler)
-    key.addEventListener('contextmenu', contextMenuHandler)
+    key.addEventListener('mousedown', downHandler);
+    key.addEventListener('mouseup', upHandler);
+    key.addEventListener('touchstart', downHandler);
+    key.addEventListener('touchend', upHandler);
+    key.addEventListener('contextmenu', contextMenuHandler);
   }
     
   function keyDown(e) {
     if (event.isComposing || event.repeat || event.keyCode === 229) {
       return;
     }
-    i = keys.findIndex(k => k == event.key)
+    i = keys.findIndex(k => k == event.key);
     if (i == -1) {
-      return
+      return;
     }
-    dispatchMIDI(input, makeMIDIData(channel, 57 + i, 64))
+    dispatchMIDI(input, makeMIDIData(channel, 57 + i, 64));
   } 
 
   function keyUp(e) {
     if (event.isComposing || event.keyCode === 229) {
       return;
     }
-    i = keys.findIndex(k => k == event.key)
+    i = keys.findIndex(k => k == event.key);
     if (i == -1) {
-      return
+      return;
     }
-    dispatchMIDI(input, makeMIDIData(channel, 57 + i, 0))
+    dispatchMIDI(input, makeMIDIData(channel, 57 + i, 0));
   } 
 
   document.addEventListener("keydown", keyDown);
@@ -207,7 +209,7 @@ async function createBuiltinSynth() {
       filter = audioCtx.createBiquadFilter();
       filter.type = "lowpass";
       filter.frequency.setValueAtTime(1000, audioCtx.currentTime);
-      filter.Q.setValueAtTime(10, audioCtx.currentTime)
+      filter.Q.setValueAtTime(10, audioCtx.currentTime);
       filter.connect(analyzer);
 
       const sinusoidNode = new AudioWorkletNode(audioCtx, "sinusoid-processor");
@@ -239,7 +241,7 @@ async function createBuiltinSynth() {
         return;
       }
       if (node != null) {
-        node.port.postMessage(data)
+        node.port.postMessage(data);
       }
     }
   }
@@ -297,43 +299,43 @@ async function createBuiltinSynth() {
   }
 
   requestAnimationFrame(draw);
-  return registerOutput("Builtin Synthesizer", handleMIDIMessage)
+  return registerOutput("Builtin Synthesizer", handleMIDIMessage);
 }
 
 function registerWebMIDIPort(port) {
   if ('uid' in port) {
-    return
+    return;
   }
   
   if (port.type == "input") {
-    port.uid = registerInput(port.name)
-    const uid = port.uid
+    port.uid = registerInput(port.name);
+    const uid = port.uid;
     port.onmidimessage = function (e) {
-      dispatchMIDI(uid, e.data)
-    }
+      dispatchMIDI(uid, e.data);
+    };
   } else if (port.type == "output") {
     port.uid = registerOutput(port.name, function (data) {
-      port.send(data)
-    })
+      port.send(data);
+    });
   }
 }
 
 function unregisterWebMIDIPort(port) {
   if (port.type == "input") {
-    unregisterInput(port.uid)
-    delete port.uid
+    unregisterInput(port.uid);
+    delete port.uid;
   } else if (port.type == "output") {
-    unregisterOutput(port.uid)
-    delete port.uid
+    unregisterOutput(port.uid);
+    delete port.uid;
   }
 }
 
 function onWebMIDIStateChange(e) {
-  port = e.port
+  port = e.port;
   if (port.state == "connected") {
-    registerWebMIDIPort(port)
+    registerWebMIDIPort(port);
   } else if (port.state == "disconnected") {
-    unregisterWebMIDIPort(port)
+    unregisterWebMIDIPort(port);
   }
 }
 
@@ -345,12 +347,12 @@ async function maybeInitializeWebMIDI() {
     return;
   }
   for (let port of access.inputs.values()) {
-    registerWebMIDIPort(port)
+    registerWebMIDIPort(port);
   }
   for (let port of access.outputs.values()) {
-    registerWebMIDIPort(port)
+    registerWebMIDIPort(port);
   }
-  access.onstatechange = onWebMIDIStateChange
+  access.onstatechange = onWebMIDIStateChange;
 }
 
 async function startup() {
